5f0382f92c67f30317ade3f3c1857990a1ded770
Garp02 <mr.ibra@ciencias.unam.mx>
2025-03-20 16:02:30 -0600

    Se creó la clase Manecilla para su implementación en la clase Reloj.

---
+++ Practica_2/Manecilla.java
@@ -0,0 +1,22 @@
+public class Manecilla
+{
+    private int valor;
+    
+    // Constructor
+    public Manecilla(int valor)
+    {
+        this.valor = 0;
+    }
+
+    // Método de acceso
+    public int getValor()
+    {
+        return this.valor;
+    }
+
+    // Método mutante
+    public void setValor(int valor)
+    {
+        this.valor = valor;
+    }
+}
