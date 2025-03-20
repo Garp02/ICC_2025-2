871d69d987b94f6f05b2ce5b277ecf98ef5da08a
Garp02 <mr.ibra@ciencias.unam.mx>
2025-03-20 16:03:22 -0600

    Se implementó la clase Manecilla, usando instancias como atributos de la clase Reloj.

--- Practica_2/Reloj.java
+++ Practica_2/Reloj.java
@@ -3,76 +3,74 @@ public class Reloj
     // Atributos
     private static final String RESET = "\u001B[0m";
     private static final String GREEN = "\u001B[32m";
-    private static final String RED = "\u001B[31m";
-    private static final String YELLOW = "\u001B[33m";
-    private int hora;
-    private int minuto;
-    private int segundo;
+    private Manecilla manecillaHora;
+    private Manecilla manecillaMinuto;
+    private Manecilla manecillaSegundo;
 
     // Constructor
     public Reloj(int hora, int minuto, int segundo)
     {
-        this.hora = hora;
-        this.minuto = minuto;
-        this.segundo = segundo;
+        this .manecillaHora = new Manecilla(hora);
+        this .manecillaMinuto = new Manecilla(minuto);
+        this .manecillaSegundo = new Manecilla(segundo);
     }
 
     // Métodos de acceso
     public int getHora()
     {
-        return this.hora;
+        return this.manecillaHora.getValor();
     }
-    
+
     public int getMinuto()
     {
-        return this.minuto;
+        return this.manecillaMinuto.getValor();
     }
 
     public int getSegundo()
     {
-        return this.segundo;
+        return this.manecillaSegundo.getValor();
     }
 
     // Métodos mutantes
     public void setHora(int hora)
     {
-        this.hora = hora;
+        this.manecillaHora.setValor(hora);
     }
 
     public void setMinuto(int minuto)
     {
-        this.minuto = minuto;
+        this.manecillaMinuto.setValor(minuto);
     }
 
     public void setSegundo(int segundo)
     {
-        this.segundo = segundo;
+        this.manecillaSegundo.setValor(segundo);
     }
 
     // Métodos de clase
     public void avanzarHora(int horas)
     {
-        setHora((this.hora + horas) % 24);
+        setHora((this.manecillaHora.getValor() + horas) % 24);
     }
 
     public void avanzarMinuto(int minutos)
     {
-        setMinuto(this.minuto + minutos);
+        setMinuto(this.manecillaMinuto.getValor() + minutos);
 
-        while(this.minuto >= 60)
+        while(this.manecillaMinuto.getValor() >= 60)
         {
-            this.minuto -= 60;
+            this.manecillaMinuto.setValor(this.manecillaMinuto.getValor() - 60);
             avanzarHora(1);
         }
     }
 
     public void avanzarSegundo(int segundos)
     {
-        setSegundo(this.segundo + segundos);
+        setSegundo(this.manecillaSegundo.getValor() + segundos);
 
-        while(this.segundo >= 60)
+        while(this.manecillaSegundo.getValor() >= 60)
         {
-            this.segundo -= 60;
+            this.manecillaSegundo.setValor(this.manecillaSegundo.getValor() - 60);
             avanzarMinuto(1);
         }
     }
@@ -87,9 +85,9 @@ public class Reloj
     public String darHora()
     {
         return String.format(
-        "%s%02d%s:%s%02d%s:%s%02d%s", 
-        GREEN, this.hora, RESET, 
-        RED, this.minuto, RESET, 
-        YELLOW, this.segundo, RESET);
+        "%s%02d:%02d:%02d%s", 
+        GREEN, this.manecillaHora.getValor(), 
+        this.manecillaMinuto.getValor(), 
+        this.manecillaSegundo.getValor(), RESET);
     }
 }
