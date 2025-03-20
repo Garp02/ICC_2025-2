c2b1a45bac8e89c31a5d4a9a2c95251d8a06ad32
Garp02 <mr.ibra@ciencias.unam.mx>
2025-03-20 16:01:03 -0600

    Se mejoró el formato y se agregó un mensaje final.

--- Practica_2/Main.java
+++ Practica_2/Main.java
@@ -12,12 +12,13 @@ public class Main
     public static void main(String[] reloj)
     {
         int opcionRepetir = 1;
+        
         Scanner scanner = new Scanner(System.in);
-        Reloj reloj1 = new Reloj(12, 0, 0);
+        Reloj reloj1 = new Reloj(0,0,0);
 
         do
         {
-            System.out.println("\n\t\t Reloj \n");
+            System.out.println("\n\t\t  Reloj \n");
             System.out.println("\t\t" + reloj1.darHora());
 
             System.out.println("\n1. Actualizar horas");
@@ -34,45 +35,51 @@ public class Main
                 case 1:
                 System.out.println("\n\tActualizar horas");
                 System.out.println("\nHora previa: " + reloj1.darHora());
+
                 System.out.print("\nIngrese las horas a actualizar: ");
                 int horas = scanner.nextInt();
+
                 reloj1.avanzarHora(horas);
                 System.out.println("\nHora actualizada: " + reloj1.darHora());
-                
                 break;
                 
                 case 2:
                 System.out.println("\n\tActualizar minutos");
                 System.out.println("\nHora previa: " + reloj1.darHora());
+
                 System.out.print("\nIngrese los minutos a actualizar: ");
                 int minutos = scanner.nextInt();
+                
                 reloj1.avanzarMinuto(minutos);
                 System.out.println("\nHora actualizada: " + reloj1.darHora());
-                
                 break;
                 
                 case 3:
                 System.out.println("\n\tActualizar segundos");
                 System.out.println("\nHora previa: " + reloj1.darHora());
+                
                 System.out.print("\nIngrese los segundos a actualizar: ");
                 int segundos = scanner.nextInt();
+                
                 reloj1.avanzarSegundo(segundos);
                 System.out.println("\nHora actualizada: " + reloj1.darHora());
-
                 break;
                 
                 case 4:
                 System.out.println("\n\tEstablecer hora");
+
                 System.out.println("\nHora previa: " + reloj1.darHora());
                 System.out.print("\nIngrese la hora: ");
                 int hora = scanner.nextInt();
+                
                 System.out.print("\nIngrese el minuto: ");
                 int minuto = scanner.nextInt();
+                
                 System.out.print("\nIngrese el segundo: ");
                 int segundo = scanner.nextInt();
+                
                 reloj1.fijarHora(hora, minuto, segundo);
                 System.out.println("\nHora actualizada: " + reloj1.darHora());
-
                 break;
                 
                 default:
@@ -84,6 +91,9 @@ public class Main
             opcionRepetir = scanner.nextInt();
         }
         while (opcionRepetir != 0);
+
+        System.out.println("\n\tFin del programa\n");
+        
         scanner.close();
     }
 }
